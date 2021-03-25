104298854af78cbd9094e8ecfd3641735737ba6b
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-25 18:42:25 +0100

    All ok (ralazioni not working)

---
+++ resources/views/guest/post/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title','elenco dei post')
+
+@section('content')
+<h1>elenco post</h1>
+<div class="container">
+  @foreach($posts as $post)
+  <div class="card">
+  <div class="card-header">
+    {{$post->title}}
+  </div>
+  <div class="card-body">
+    <p class="card-text">{{$post->content}}</p>
+    <a href="{{ route('guest.posts.show', $post->slug)}}" class="btn btn-primary">Dettagli</a>
+  </div>
+</div>
+  @endforeach
+</div>
+@endsection
--- resources/views/guest/post/show.blade.php
+++ resources/views/guest/post/show.blade.php
@@ -1,1 +1,17 @@
+@extends('layouts.app')
 
+@section('title','dettaglio post')
+
+@section('content')
+<h1>dettaglio post</h1>
+<div class="container">
+  <div class="card">
+  <div class="card-header">
+    {{$post->title}}
+  </div>
+  <div class="card-body">
+    <p class="card-text">{{$post->content}}</p>
+  </div>
+</div>
+</div>
+@endsection
