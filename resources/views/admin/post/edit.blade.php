c4750e6c81040a2fd6396b6154272c3e25e7e50e
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-29 19:25:51 +0200

    init IMG

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
 
-<form action="{{route('post.update', $post)}}" method="post">
+<form action="{{route('post.update', $post)}}" method="post" enctype="multipart/form-data">
   @csrf
   @method('PUT')
 
@@ -9,6 +9,13 @@
     <label for="title">Title</label>
     <input type="text" class="form-control" id="title" name="title" value="{{$post->title}}">
   </div>
+
+  <form>
+    <div class="form-group">
+      <label for="immagine">Carica l'immagine</label>
+      <input type="file" class="form-control-file" id="immagine" name="image">
+    </div>
+  </form>
 
   <div class="form-group">
     <label for="body">Body</label>
